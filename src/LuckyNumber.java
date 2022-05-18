14b127de82ac17dbe1d13640d6ea4bd26fa972a2
Abhishek <jadonabhi0@gmail.com>
2022-05-19 01:22:03 +0530

    updated

---
+++ src/LuckyNumber.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LuckyNumber {
+    static LinkedList<Integer> list =  new LinkedList<>();
+    static List<Integer> luckynumber(List<Integer> arr){
+        int counter = 0;
+        int count = 0;
+        for(int i : arr){
+            for(int j = 1 ; j <= i ; j++){
+                counter++;
+                if(counter%j==0){
+                    count++;
+                }
+            }
+            if(count == i){
+                list.add(count);
+                count =0 ;
+            }else{
+                count = 0;
+            }
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> list = new LinkedList<>();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+//        luckynumber(list).forEach(System.out::println);
+        System.out.println(12);
+
+    }
+
+}
---
+++ src/MatrixDiagonal.java
@@ -0,0 +1,30 @@
+public class MatrixDiagonal {
+    public static int diagonalSum(int[][] mat){
+        int n = mat.length;
+        int sum1 = 0 ;
+        int sum2 = 0;
+        int total = 0;
+        int temp = 0;
+        for (int i = 0 ; i < n; i++){
+            for (int j = 0 ; j < n ; j++){
+                if(i == j){
+                    sum1+=mat[i][j];
+                }
+                if (i+j == n-1){
+                    sum2 += mat[i][j];
+                }
+            }
+        }
+        if (n%2!=0){
+            temp = mat[n/2][n/2];
+            total = sum1+sum2-temp;
+        }else{
+            total = sum1+sum2;
+        }
+        return total;
+    }
+    public static void main(String[] args) {
+        int[][] array = new int[][]{{1,2,3},{2,3,4},{1,5,6}};
+        System.out.println(diagonalSum(array));
+    }
+}
