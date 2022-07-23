a3b6d0983975b11a8a5431130e0b222d047cb855
Abhishek <jadonabhi0@gmail.com>
2022-07-24 03:22:46 +0530

    updated

--- src/SortAman.java
+++ src/SortAman.java
@@ -25,10 +25,10 @@ public class SortAman {
         }
     }
 
-    public static void helper(List<Ruff.sort> list){
-        Collections.sort(list, Comparator.comparing(Ruff.sort::getQuantity).thenComparing(Ruff.sort::getPrice).thenComparing(Ruff.sort::getName).reversed());
+    public static void helper(List<sort> list){
+        Collections.sort(list, Comparator.comparing(sort::getQuantity).thenComparing(sort::getPrice).thenComparing(sort::getName).reversed());
 
-        for(Ruff.sort obj : list){
+        for(sort obj : list){
             System.out.print(obj.name+" "+obj.quantity+" "+obj.price);
             System.out.println();
         }
@@ -37,13 +37,13 @@ public class SortAman {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Ruff.sort> list = new LinkedList<>();
+        List<sort> list = new LinkedList<>();
         int n = sc.nextInt();
         for(int i = 0 ; i < n ; i++){
             String name = sc.next();
             int quantity = sc.nextInt();
             int price = sc.nextInt();
-            list.add(new Ruff.sort(name,quantity,price));
+            list.add(new sort(name,quantity,price));
         }
         helper(list);
     }
